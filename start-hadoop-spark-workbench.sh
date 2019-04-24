f4c590e4a40646a14528681fe352a0dbebb1ec04
danielchen89 <35557565+danielchen89@users.noreply.github.com>
2019-04-24 12:25:54 +0800

    Add files via upload

--- start-hadoop-spark-workbench.sh
+++ start-hadoop-spark-workbench.sh
@@ -2,7 +2,7 @@
 
 docker-compose up -d
 
-my_ip=`curl ifconfig.me/ip`
+my_ip=`ip route get 1|awk '{print $NF;exit}'`
 echo "Namenode: http://${my_ip}:50070"
 echo "Datanode: http://${my_ip}:50075"
 echo "Spark-master: http://${my_ip}:8080"
